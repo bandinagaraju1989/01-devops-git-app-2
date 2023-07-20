04715c88b79c3dab48bb43ac229fcf79a8310c86
pharmatosoftware <pharmatosoftware1123@gmail.com>
2023-07-20 19:55:58 +0530

    first commit

---
+++ src/main/java/maven/group/maven/jar/app1/MavenJarApp1Application.java
@@ -0,0 +1,13 @@
+package maven.group.maven.jar.app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenJarApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJarApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/maven/group/maven/jar/app1/MavenJarApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package maven.group.maven.jar.app1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJarApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
